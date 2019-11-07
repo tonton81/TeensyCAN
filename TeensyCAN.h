fe5c9381c147e46ca22abacb8fef7a1a6cc2c943
Antonio Brewer <yodabox64@hotmail.com>
2019-11-07 03:24:55 -0500

    Update TeensyCAN.h

--- TeensyCAN.h
+++ TeensyCAN.h
@@ -10,7 +10,7 @@
 
 struct AsyncTC {
   uint8_t node = 0;
-  uint8_t packetid = 0;
+  uint16_t packetid = 0;
   bool broadcast = 0;
 };
 
@@ -31,32 +31,46 @@ class TeensyCAN {
     static uint8_t nodeID;
     static void onReceive(_tC_ptr handler) { TeensyCAN::_handler = handler; }
     static _tC_ptr _handler;
-    TeensyCAN() { Serial.featuredNode = 0;}
     static FlexCAN_T4_Base* _flexcanPtr;
 
     class NodeFeatures : public Stream {
       public:
         NodeFeatures(){;}
-        virtual size_t print(const char *p) {return 0;}
-        virtual size_t println(const char *p){return 0;}
-        virtual size_t write(uint8_t val) {return 0;}
-        virtual size_t write(const char *buf, size_t size) {return 0;}
-        virtual size_t write(const uint8_t *buf, size_t size){return 0;}
-        virtual int available(){return 0;}
-        virtual int peek(){return 0;}
-        virtual int read(){return 0;}
+        virtual size_t print(const char *p) { return write((const uint8_t*)p, strlen(p)); }
+        virtual size_t println(const char *p);
+        virtual size_t write(uint8_t val) { return write(&val, 1); }
+        virtual size_t write(const char *buf, size_t size) { return write((uint8_t*)buf,size); }
+        virtual size_t write(const uint8_t *buf, size_t size);
+        virtual int available();
+        virtual int peek();
+        virtual int read();
         virtual size_t read(uint8_t* buf, size_t size){return 0;}
-        virtual void flush() {return;}
+        virtual size_t readBytes(uint8_t* buf, size_t size);
+        virtual void flush() {;} // unused
 
       private:
+        void sendControl(const uint8_t *array, uint16_t len, uint8_t port, uint16_t packetid, uint8_t toNode, uint8_t delay_send = 0, uint32_t timeout = 500);
         uint8_t featuredNode;
-        uint8_t serial_access;
-        uint8_t spi_access;
-        uint8_t wire_access;
+        uint8_t serial_access = 0x80;
+        uint8_t spi_access = 0x80;
+        uint8_t wire_access = 0x80;
         uint8_t port; // serial/wire/spi port
         friend TeensyCAN;
     };
     NodeFeatures Serial;
+    NodeFeatures Serial1;
+    NodeFeatures Serial2;
+    NodeFeatures Serial3;
+    NodeFeatures Serial4;
+    NodeFeatures Serial5;
+    NodeFeatures Serial6;
+    NodeFeatures Wire;
+    NodeFeatures Wire1;
+    NodeFeatures Wire2;
+    NodeFeatures Wire3;
+    NodeFeatures SPI;
+    NodeFeatures SPI1;
+    NodeFeatures SPI2;
 
 
 
@@ -64,6 +78,7 @@ class TeensyCAN {
   static volatile int payload_ack_check;
 
   private:
+
 };
 
 extern TeensyCAN Node;
